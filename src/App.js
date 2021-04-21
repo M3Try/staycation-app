57792637449cdb2b5aebbd0c7f767d15d88988aa
dagowaksdev <try.nothing@gmail.com>
2021-04-21 22:04:40 +0700

    next commit adding some components Buttons with Testing on staycation app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './assets/scss/style.scss';
+
+function App() {
+  return (
+    <div className="App">Hai Aku Lagi Bikin Project Baru Nih ..... !
+    </div>
+  );
+}
+
+export default App;
---
+++ src/elements/Button/index.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+
+import Button from './index';
+
+test('should not allowed click button if isDisabled is present', () => {
+    const { container } = render(<Button isDisabled></Button>);
+
+    expect(container.querySelector('span.disabled')).toBeInTheDocument()
+});
+
+test('should render loading/spinner ', () => {
+    const { container, getByText } = render(<Button isLoading></Button>);
+
+    expect(getByText(/loading/i)).toBeInTheDocument();
+    expect(container.querySelector('span')).toBeInTheDocument();
+});
+
+test('should render <a> tag', () => {
+    const { container } = render(<Button type='link' isExternal></Button>);
+
+    expect(container.querySelector('a')).toBeInTheDocument();
+});
+
+test('should render <Link> components ', () => {
+    const { container } = render(
+        <Router><Button href='' type='link'></Button></Router>
+    );
+
+    expect(container.querySelector('a')).toBeInTheDocument();
+});
+
+
+
